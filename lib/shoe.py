f5229169643cf0b44bd13a90b7eb4fd3c7151050
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-06 17:45:44 +0300

    adds code for oop putting it all together

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        """says that the shoe has been repaired"""
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
